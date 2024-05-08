f6d33972f09f9515471644b404955acda8a27531
Lefchakk <joao.lefchak@edu.unipar.br>
2024-05-08 20:47:24 -0300

    scripts

---
+++ Galeria de imagem/scripts.basico.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+alert("erro 123");
+
+//Tipos de dados
+//string: Sequencia de caracter
+//Number: Números inteiros ou pontos flutuantes (10.4)
+//Boolean: Retorna dois valores, true ou false
+//Object: Estrutura de dados que pode armazenar múltiplos valores
+//NULL: Quando está vazio
+//Array: Objeto especial ordenado
+//Function: Bloco de código reutilizável.
+
+//CONST É usado para declarar uma variável cujo valor é permanente
+    const teste = 50;
+    
+})
