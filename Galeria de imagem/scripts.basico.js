f5d9769634d70e09aabb68da74f13a4a1899aef3
Lefchakk <joao.lefchak@edu.unipar.br>
2024-05-15 20:50:03 -0300

    novas scripts

--- Galeria de imagem/scripts.basico.js
+++ Galeria de imagem/scripts.basico.js
@@ -8,15 +8,15 @@ document.addEventListener('DOMContentLoaded', function(){
 //NULL: Quando está vazio
 //Array: Objeto especial ordenado
 //Function: Bloco de código reutilizável.
-
 //CONST É usado para declarar uma variável cujo valor é permanente
+
     const teste = 50;
 
     //LET é usado para declarar variáveis que os valores podem ser retibruidos
 
     let C = 0;
     C - 10;
-    console.log(C);
+    //console.log(C);
 
     //const - variavel;
     const notaMedia = 5;
@@ -28,11 +28,11 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     //else if - condição falsa, usar em conjunto.  
     else if (notaMedia >= 4 && notaMedia < 6) {
-        alert("Aluno quase passou!");
+       // alert("Aluno quase passou!");
 
     }
     else { 
-        alert("Aluno reprovou!")
+       // alert("Aluno reprovou!")
     }
 
     //------ OPERADORES -------
@@ -50,7 +50,57 @@ document.addEventListener('DOMContentLoaded', function(){
         console.log ("sim é Lucas");
     }
     else{
-        console.log ("não é Lucas");
+       // console.log ("não é Lucas");
+    }
+    //-----------------//-------------//---------------//---------------
+
+
+    //Funções são os blocos de código que podem ser chamados e reutilizados
+    //Dentro dos parenteses da função vai os parâmetros/propriedades
+
+    //CONST É usado para declarar uma variável cujo valor é permanente
+
+    function primeiraFuncao (parametro, segundoParametro){
+        return parametro + segundoParametro //duas variaveis "concatecadas ( ++ ) "
     }
 
+   const parametro = "Essa é uma função com parâmetro,"
+   const segundoParametro = " agora com dois parãmetros"
+    
+    //console.log(primeiraFuncao(parametro, segundoParametro));
+
+    //-------------//------------//--------------//----------------
+
+    //arrays em javascript são estruturas de dados que permitem armazenar uma 
+    //coleção ordenada de elementos.
+    //tem que ficar dendro de [ ]
+
+    //Array: Objeto especial ordenado iniciando sempre da "chave 0"
+    
+
+    let primeiroArray = [ "João" , "Lucas" , "Paulo" , "Pedro"]
+
+    //console.log (primeiroArray[ 1 ])
+
+    //---------------//-----------//----------//---------------
+
+    //Os objetos são coleções NÃO ORDENADAS de pares chave-valor,
+    //onde cada valor é associado a uma chave única
+
+    let pessoa = {
+        nome: "João",
+        idade: "22",
+        profissao: "Programador",
+    }
+    
+    //console.log(pessoa["nome"])
+
+    //------------//------------//--------------//-------
+
+    const numeros = [ "A", "B", "C", "D"]
+
+    numeros.forEach( num => {
+        console.log (num )
+
+    })
 })  
