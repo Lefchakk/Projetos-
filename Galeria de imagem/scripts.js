a53c863094b1c9f1883d8fe7881dacccfca09b00
Lefchakk <joao.lefchak@edu.unipar.br>
2024-05-22 20:47:17 -0300

    Galeria imagens

--- Galeria de imagem/scripts.js
+++ Galeria de imagem/scripts.js
@@ -1,19 +1,32 @@
 document.addEventListener('DOMContentLoaded', function (){
 
     const baseImagens = { 
-        todas: ["imagem.jpg", "cidade.png","animal.jpg","natureza.jpg"],
-        natureza:[],
-        cidade:[],
-        animais:[],
+        todas: ["cidade.jpg","animais.png","natureza.png"],
+        natureza:["natureza.png"],
+        cidade:["cidade.jpg"],
+        animais:["animais.png"],
             
     }
+
+    function carregaImagens(){
+        const galeria = document.querySelector("#galeria-imagens"); //Selecionar elementos HTML com base em um seletor
+        
+        const imagens = baseImagens["todas"]
+
+        imagens.forEach(img => { 
+                console.log(img);     
+                galeria.innerHTML += '<div> <img src="imagens/'+ img + '" </div>';                
+        })
+        
+    }
+    carregaImagens();
 
     //Evento de click
     document.body.addEventListener('click', function (event){ 
         //acessa a lista de classe de onde clicou e 
         //procurra pela classe botao-categoria
         if(event.target.classList.contains("botao-categoria")){
-         alert(123);
+         //alert(123);
         }
     })
 
