6082c7fade4f7e32215f3a460b59f0918b6cb36b
Lefchakk <joao.lefchak@edu.unipar.br>
2024-05-29 20:54:32 -0300

    Scripts Button

--- Galeria de imagem/scripts.js
+++ Galeria de imagem/scripts.js
@@ -1,32 +1,58 @@
 document.addEventListener('DOMContentLoaded', function (){
 
     const baseImagens = { 
-        todas: ["cidade.jpg","animais.png","natureza.png"],
+        todas: ["cidade.jpg","animais.png","natureza.png","pesquisa"],
         natureza:["natureza.png"],
         cidade:["cidade.jpg"],
         animais:["animais.png"],
             
     }
 
-    function carregaImagens(){
-        const galeria = document.querySelector("#galeria-imagens"); //Selecionar elementos HTML com base em um seletor
-        
-        const imagens = baseImagens["todas"]
+    function carregaImagens(categoria){
+        //Selecionar elementos HTML com base em um seletor
+        const galeria = document.querySelector("#galeria-imagens"); 
+        galeria.innerHTML="";
+
+        const imagens = baseImagens[categoria]
 
         imagens.forEach(img => { 
-                console.log(img);     
-                galeria.innerHTML += '<div> <img src="imagens/'+ img + '" </div>';                
+                galeria.innerHTML += '<div class="imagem-item"> <img src="imagens/'+ img + '" </div>';                
         })
         
     }
-    carregaImagens();
+
+    function ordenaImagens(ordem){
+
+        const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"))
+
+        imagens.sort((a,b) => {
+            console.log(a)
+
+
+        })
+    }
+
+
+
+    carregaImagens("todas");
 
     //Evento de click
     document.body.addEventListener('click', function (event){ 
         //acessa a lista de classe de onde clicou e 
         //procurra pela classe botao-categoria
         if(event.target.classList.contains("botao-categoria")){
-         //alert(123);
+        
+        const categoria = event.target.dataset.categoria;
+
+        //alert(categoria);
+         carregaImagens(categoria);
+            
+        }
+        if(event.target.classList.contains("botao-ordenar")){
+
+            const ordem = event.target.dataset.ordem;
+            ordenaImagens(ordem)
+
         }
     })
 
