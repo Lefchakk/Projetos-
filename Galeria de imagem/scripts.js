592b609fccad55e3a8480e5056e226ba476987ae
Lefchakk <joao.lefchak@edu.unipar.br>
2024-06-05 20:47:38 -0300

    Finalize

--- Galeria de imagem/scripts.js
+++ Galeria de imagem/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (){
 
     const baseImagens = { 
-        todas: ["cidade.jpg","animais.png","natureza.png","pesquisa"],
+        todas: ["cidade.jpg","animais.png","natureza.png"],
         natureza:["natureza.png"],
         cidade:["cidade.jpg"],
         animais:["animais.png"],
@@ -16,40 +16,41 @@ document.addEventListener('DOMContentLoaded', function (){
         const imagens = baseImagens[categoria]
 
         imagens.forEach(img => { 
-                galeria.innerHTML += '<div class="imagem-item"> <img src="imagens/'+ img + '" </div>';                
+                galeria.innerHTML += '<div class="imagem-item"> <img src="imagens/'+ img + ' "alt="'+ img +'" </div>';                
         })
         
     }
 
     function ordenaImagens(ordem){
+       
 
         const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"))
 
         imagens.sort((a,b) => {
-            console.log(a)
+            const nameA = a.querySelector('img').getAttribute('alt');
+            const nameB = b.querySelector('img').getAttribute('alt');
 
+           //operador ternário - se for true ? se nao:
+            return ordem == 'asc' ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA)
+        })
 
+        const galeria= document.getElementById('galeria-imagens')
+        galeria.innerHTML = ''; //esvazia  box das imagens
+        imagens.forEach(imagem => {
+            galeria.appendChild(imagem); //adiciona as imagens novamente, agora ordenada  
         })
     }
-
-
-
     carregaImagens("todas");
 
     //Evento de click
-    document.body.addEventListener('click', function (event){ 
+    document.body.addEventListener("click", function (event){ 
         //acessa a lista de classe de onde clicou e 
         //procurra pela classe botao-categoria
         if(event.target.classList.contains("botao-categoria")){
-        
-        const categoria = event.target.dataset.categoria;
-
-        //alert(categoria);
-         carregaImagens(categoria);
-            
+             const categoria = event.target.dataset.categoria;
+             carregaImagens(categoria);        
         }
         if(event.target.classList.contains("botao-ordenar")){
-
             const ordem = event.target.dataset.ordem;
             ordenaImagens(ordem)
 
